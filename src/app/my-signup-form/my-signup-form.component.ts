1d013d2ec39830ae6ee3f9f3da7f4ed259f61f34
Manuel <m.a.c.p90@gmail.com>
2017-11-06 18:59:16 -0500

    firs commit

---
+++ src/app/my-signup-form/my-signup-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-signup-form',
+  templateUrl: './my-signup-form.component.html',
+  styleUrls: ['./my-signup-form.component.css'],
+  // encapsulation: ViewEncapsulation.None
+})
+export class MySignupFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
